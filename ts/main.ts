bdca30d99188ac9b43eb9d5c9cf8d5fe124f9660
Deivintir <d4vidyeva@gmail.com>
2024-02-10 20:53:20 +0100

    Add Herencia de clases en POO

---
+++ ts/main.ts
@@ -0,0 +1,28 @@
+/**Herencia y composición de clases en TypeScript:
+ * Concluimos el módulo con una característica esencial en la programación orientada a objetos como es el uso de herencia, que permite la reutilización de código y el
+ * establecimiento de una jerarquía de clases.
+ * 
+ * Comenzaremos conociendo qué es la herencia y qué terminología lleva asociada para seguidamente aprender de forma práctica cómo utiliza TypeScript las interfaces en
+ * cuanto a su implementación en clases.
+ * 
+ * Posteriormente, aprenderemos cómo se implementa la herencia en TypeScript realizando varias prácticas relacionadas con las superclases y las subclases, y qué especi_
+ * ficaciones resultan en el caso de las clases abstractas.
+ * 
+ * Para concluir, estudiaremos de forma práctica una alternativa a la herncia, la composición de clases; cubriremos de esta manera los principales casos de uso de estas
+ * características en TypeScript. */
+/**---------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+/**---------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+/**Herencia de clases en POO:
+ * Una de las características fundamentales de la programación orientada a objetos es la herencia, un mecanismo que permite que una clase puede heredar de otra sus miembros,
+ * propiedades y métodos, con el objetivo de reutilizar código, generando así una jerarquía de clases dentro de una solución de software.
+ * 
+ * Si una clase deriva de otra, hereda sus propiedades y métodos, y puede añadir nuevas propiedades y métodos, o redefinir los heredados. En programación orientada a objetos,
+ * se define la siguiente terminología para esta característica de herencia de clases. 
+ * Superclase: También conocida como clase base o principal, es aquella de la que otras clases heredan sus métodos o propiedades.
+ * Subclase. También conocida como clase derivada, extendida o hija, es aquella que hereda los métodos o propiedades de una superclase.
+ * Reutilización: Implementación que permite reutilizar código en los programas mejorando su limpieza y mantenimiento evolutivo.
+ * 
+ * Aunque la herencia en JavaScript es una característica incorporada a partir de ECMAScript 2015, es en TypeScript donde adopta un papel relevante, pues permite ser combinada
+ * con el tipado estático propio de este superset, como veremos en los siguiente casos de uso.*/
+/**---------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+/**---------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
